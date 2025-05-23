b419a52cd3b9d2a14e6066fdacc5a3ecc7415b18
Masc20 <melecioandrescabahug@gmail.com>
2025-05-23 19:13:02 +0800

    aa

--- public/main.php
+++ public/main.php
@@ -2,3 +2,15 @@
 echo "pag practice na lagig git BAHAHAHHAHAHAHA ug ari ka mo commit sigurado pa HAHAHAH"; 
 echo "hello madaam";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
